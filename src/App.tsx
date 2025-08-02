64d01ad0f04d6c0f62e73d2b25b89412643e60a2
Jorge Daniel <dev.jdaniel@gmail.com>
2025-08-02 12:23:34 -0500

    feat: Added useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+'use client'
+
+import { useTheme } from '@/hooks/useTheme'
+
+import { Button } from './components/shadcn'
+
 export const App = () => {
-  return <h1 className="text-3xl font-bold underline">App</h1>
+  const { theme, toggleTheme } = useTheme()
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <Button onClick={toggleTheme}>
+        {theme === 'light' ? 'Light' : 'Dark'}
+      </Button>
+    </h1>
+  )
 }
